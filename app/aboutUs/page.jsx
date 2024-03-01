d32e224f49ef8c405ba356b756b863208f00965d
Aniyeshu-31 <aniyeshu31@gmail.com>
2024-03-01 21:47:05 +0530

    updated!

---
+++ app/aboutUs/page.jsx
@@ -0,0 +1,57 @@
+'use client'
+import styles from './aboutus.module.css'
+const AboutUs = () => {
+  return (
+    <div className={styles.container}>
+      <a
+        href="#"
+        className={` ${styles.btn} ${styles.previous} ${styles.round}`}
+      >
+        &#8249;
+      </a>
+      <h1>About Us</h1>
+      <div className={styles.content}>
+        Welcome to Less Pay, your number one solution of your daily payments.
+        We're dedicated to providing you with the best of the services, with a
+        focus on dependability. customer service, and your benefits.
+        <br />
+        <br />
+        Less Pay is FinTech Startup Based in Chandigarh aimed to uplift the
+        local and small businesses. Gastos is an Application based startup which
+        helps user to save their money while doing payments at their local shops
+        and businesses also helping them to manage all of their expenses at one
+        place. <br />
+        Gastos has a potential of becoming a Billion Dollar Industry in coming
+        years and has a potential user base of more than 200 Million to target.
+        Gastos is the only startup in India which is bringing UPI Digital cards
+        for the users which helps them to save their money.
+        <br />
+        <br />
+        During this Covid-19 situation time and between the hard races of online
+        delivery, the local and small scaled businesses are mostly affected.
+        Gastos is the startup which is promoting these small businesses to grow
+        and become a brand before the user. Gastos is solving multiple problems
+        for users like fake discounts, lack of saving, unmanaged expenses.
+        <br />
+        <br />
+        We're working to turn our passion for the services into a booming Market
+        Network. We hope you enjoy our services as much as we enjoy offering
+        them to you.
+        <br />
+        <br />
+        <br />
+        Sincerely,
+        <br />
+        Sahgal Kumar
+        <br />
+        (Founder)
+        <br />
+        <br />
+        here are some of the following ways through which you will be connected
+        with us
+      </div>
+    </div>
+  )
+}
+
+export default AboutUs
---
+++ app/grievancepolicy/page.jsx
@@ -0,0 +1,62 @@
+'use client'
+
+import styles from './grievance.module.css'
+const AboutUs = () => {
+  return (
+    <div className={styles.container}>
+      <a
+        href="#"
+        className={` ${styles.btn} ${styles.previous} ${styles.round}`}
+      >
+        &#8249;
+      </a>
+      <h1>Grievance Policy</h1>
+      <div className={styles.content}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam,
+        accusamus nam! Deserunt cupiditate eum fugiat molestiae beatae eligendi
+        illo.
+        <br />
+        <br />
+        Quae numquam veniam eveniet, saepe officia corrupti! Culpa adipisci
+        nesciunt esse.
+        <br />
+        <br />
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea odio sint
+        ipsum cupiditate non eaque ipsa sit accusamus! Fugit provident ipsa
+        nesciunt consequatur enim at placeat atque, voluptatibus reprehenderit
+        deleniti!
+        <br />
+        <br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, eum
+        minima, aliquam fugit sequi eos possimus sit repellat commodi maiores
+        beatae pariatur inventore doloribus unde officia harum fugiat. Minima,
+        nisi!
+        <br />
+        <br />
+        <br />
+        <br />
+        <h1 className={styles.title}>Grievance Redressal</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
+        temporibus assumenda autem, sit, fuga deleniti error, iure a ipsa
+        quisquam expedita! Aliquam doloremque repellendus laboriosam non ab
+        velit inventore perspiciatis!
+        <br />
+        <br />
+        <br />
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis
+        debitis quam blanditiis pariatur reiciendis? Fugit hic earum
+        necessitatibus laudantium aspernatur eius error velit quas eligendi,
+        nihil nobis at beatae corporis?
+        <br />
+        <br />
+        <br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio cum, iure
+        deserunt distinctio unde error nesciunt magni, animi corrupti facere
+        doloremque, accusantium perferendis impedit sequi. Porro a accusantium
+        aperiam molestiae!
+      </div>
+    </div>
+  )
+}
+
+export default AboutUs
